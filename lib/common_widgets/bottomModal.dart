a322715b6f2a665cde88f15dfe010d847ad28efa
RelativeLie <sopio1992@gmail.com>
2023-04-03 15:08:47 +0300

    bottom modal refactor

--- lib/common_widgets/bottomModal.dart
+++ lib/common_widgets/bottomModal.dart
@@ -2,86 +2,61 @@ import 'package:flutter/material.dart';
 
 enum ModalSizesENUM { xs, full }
 
-// Map<ModalSizesENUM, double> modalSizes = {
-//   ModalSizesENUM.xs: 0.35,
-//   ModalSizesENUM.full: 0.95
-// };
+final Map<ModalSizesENUM, double> modalSizes = {
+  ModalSizesENUM.xs: 0.35,
+  ModalSizesENUM.full: 0.95
+};
 
-// class BottomModal extends StatefulWidget {
-//   final Widget button;
-//   final Widget modalContent;
-//   ModalSizesENUM? size;
-//   BottomModal(
-//       {required this.button,
-//       required this.modalContent,
-//       this.size = ModalSizesENUM.full,
-//       super.key});
-
-//   // const BottomModal({super.key});
-
-//   @override
-//   State<BottomModal> createState() => _BottomModalState();
-// }
-
-// class _BottomModalState extends State<BottomModal> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Center(
-//       child: ElevatedButton(
-//         style: ButtonStyle(
-//             backgroundColor: MaterialStateProperty.all(Colors.transparent),
-//             shadowColor: MaterialStateProperty.all(Colors.transparent)),
-//         onPressed: () {
-//           showModalBottomSheet<void>(
-//             context: context,
-//             isScrollControlled: true,
-//             builder: (BuildContext context) {
-//               return FractionallySizedBox(
-//                   heightFactor: modalSizes[widget.size],
-//                   child: widget.modalContent);
-//             },
-//           );
-//         },
-//         child: widget.button,
-//       ),
-//     );
-//   }
-// }
-
-class BottomModal extends StatelessWidget {
+class WdBottomModal extends StatelessWidget {
   final Widget button;
   final Widget modalContent;
-  ModalSizesENUM? size;
-  BottomModal(
+  final ModalSizesENUM? size;
+  const WdBottomModal(
       {required this.button,
       required this.modalContent,
       this.size = ModalSizesENUM.full,
       super.key});
 
-  final Map<ModalSizesENUM, double> _modalSizes = {
-    ModalSizesENUM.xs: 0.35,
-    ModalSizesENUM.full: 0.95
-  };
-
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ElevatedButton(
-        style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all(Colors.transparent),
-            shadowColor: MaterialStateProperty.all(Colors.transparent)),
-        onPressed: () {
-          showModalBottomSheet<void>(
-            context: context,
-            isScrollControlled: true,
-            builder: (BuildContext context) {
-              return FractionallySizedBox(
-                  heightFactor: _modalSizes[size], child: modalContent);
+    return Container(
+        alignment: Alignment.topLeft,
+        child: Center(
+          child: ElevatedButton(
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.transparent),
+                shadowColor: MaterialStateProperty.all(Colors.transparent)),
+            onPressed: () {
+              showModalBottomSheet<void>(
+                context: context,
+                // isScrollControlled: true,
+                builder: (BuildContext context) {
+                  return FractionallySizedBox(
+                      heightFactor: modalSizes[size],
+                      child: Container(
+                        color: Theme.of(context).colorScheme.background,
+                        child: Column(
+                          children: [
+
+                            Container(
+                              margin: EdgeInsets.only(top: 10),
+                              height: 5,
+                              width: 50,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Theme.of(context).highlightColor,
+                              ),
+                            ),
+                            Expanded(child: modalContent)
+                          ],
+                        ),
+                      )
+                      );
+                },
+              );
             },
-          );
-        },
-        child: button,
-      ),
-    );
+            child: button,
+          ),
+        ));
   }
 }
